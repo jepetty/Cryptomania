a68abcdba6054b7879f9cfcdb63a64904eacd7f9
mguida22 <michael.guida@colorado.edu>
2016-04-06 23:53:47 -0600

    Revert "updated runge-kutta, rounded values in data set"
    This reverts commit 39d3714d66e1243d14d1ba3641d8e2236bc680ed.

--- runge_kutta.m
+++ runge_kutta.m
@@ -7,7 +7,7 @@ for i = 1:n
     k3 = derivative(x0 + (h/2)*k2);
     k4 = derivative(x0 + h*k3);
     x0 = x0 + (h/6)*(k1 + 2*k2 + 2*k3 + k4);
-    x0 = round(mod(x0, 255));
+    x0 = mod(x0, 255);
     stateVector(i,:) = x0;
 end
 xState = stateVector;
