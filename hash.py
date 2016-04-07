f2726e461d5e0302d0ffdd16822772625cb2abed
mguida22 <michael.guida@colorado.edu>
2016-04-06 23:48:33 -0600

    basic hash function

---
+++ hash.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+import sys
+
+# error if no input
+if (len(sys.argv) < 2):
+    print('Usage:')
+    print('  python3 hash.py \'hello world\'')
+    sys.exit(0)
+
+# get user input
+s = sys.argv[1]
+
+# ascii value of the input string
+val = ''.join(str(ord(c)) for c in s)
+# mod by length of document to avoid going out of bounds
+val = int(val) % 499999
+
+
+i = 0
+with open('lorenz.txt') as f:
+    for line in f:
+        # print the result at the proper index
+        if (i == val):
+            print(line[:-5].replace('.', ''))
+            sys.exit(0)
+        i += 1
+
+print('Error')
