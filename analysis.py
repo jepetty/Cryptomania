e2aa46b9be966c393a3ab1d4ce4bfa0186cc4e6b
mguida22 <michael.guida@colorado.edu>
2016-04-25 18:03:55 -0600

    test cutting out first digit

--- analysis.py
+++ analysis.py
@@ -2,9 +2,23 @@
 
 import matplotlib.pyplot as plt
 
-data = []
+data_full = []
+data_trunc = []
 
-bucket = {
+bucket_full = {
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0,
+    '9': 0,
+    '0': 0,
+}
+
+bucket_trunc = {
     '1': 0,
     '2': 0,
     '3': 0,
@@ -20,12 +34,19 @@ bucket = {
 i = 0
 with open('sample.txt') as f:
     for line in f:
-        data.append((i, int(line)))
+        data_full.append((i, int(line)))
+        data_trunc.append((i, int(line[1:])))
+
+        bucket_full[line[:1]] += 1
+        bucket_trunc[line[1:2]] += 1
+
         i += 1
 
-        bucket[line[:1]] += 1
+print(bucket_full)
+print(bucket_trunc)
 
-print(bucket)
+plt.scatter(*zip(*data_full))
+plt.show()
 
-plt.scatter(*zip(*data))
+plt.scatter(*zip(*data_trunc))
 plt.show()
